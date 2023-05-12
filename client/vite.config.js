00590cbdda83ff9bb3ee65daab5607d9104e9aa8
Vinith-029 <850066798@genpact.com>
2023-05-12 11:08:16 +0530

    change

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server : {
-  //   proxy :{
-  //     '/api' : 'https://testlogiship.onrender.com'
-  //   }
-  // },
+  server : {
+    proxy :{
+      '/api' : 'https://localhost:3000'
+    }
+  },
   plugins: [react()],
   // base : "/Logiship/"
 })
